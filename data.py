72aca2d6d6cb5cd72c8c70fd7345a2144c932221
grushaprasad <grusha.prasad@gmail.com>
2018-11-21 15:59:03 -0500

    code runs

--- data.py
+++ data.py
@@ -2,61 +2,69 @@ from __future__ import print_function
 import librosa
 import pickle
 import csv
-
-filenames = ['../audio_files/CV_stimuli/a/a_dg_CV/a_dg01_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg02_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg03_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg04_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg05_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg06_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg07_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg08_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg09_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg10_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg11_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg12_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg13_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg14_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg15_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg16_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg17_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg18_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg19_CV.wav',
-    '../audio_files/CV_stimuli/a/a_dg_CV/a_dg20_CV.wav'
-]
-
-new_filenames = ['../Spectrograms/a_dg01_CV.csv',
-    '../Spectrograms/a_dg02_CV.csv',
-    '../Spectrograms/a_dg03_CV.csv',
-    '../Spectrograms/a_dg04_CV.csv',
-    '../Spectrograms/a_dg05_CV.csv',
-    '../Spectrograms/a_dg06_CV.csv',
-    '../Spectrograms/a_dg07_CV.csv',
-    '../Spectrograms/a_dg08_CV.csv',
-    '../Spectrograms/a_dg09_CV.csv',
-    '../Spectrograms/a_dg10_CV.csv',
-    '../Spectrograms/a_dg11_CV.csv',
-    '../Spectrograms/a_dg12_CV.csv',
-    '../Spectrograms/a_dg13_CV.csv',
-    '../Spectrograms/a_dg14_CV.csv',
-    '../Spectrograms/a_dg15_CV.csv',
-    '../Spectrograms/a_dg16_CV.csv',
-    '../Spectrograms/a_dg17_CV.csv',
-    '../Spectrograms/a_dg18_CV.csv',
-    '../Spectrograms/a_dg19_CV.csv',
-    '../Spectrograms/a_dg20_CV.csv'
-]
+import torch
+import os
+import numpy as np
+# directory = './audio_files/CV_stimuli/a/a_dg_CV/'
 
 
-def flatten(l):
-    return [item for sublist in l for item in sublist]
+# dat = []
 
-for i,curr_file in enumerate(filenames):
-    y, sr = librosa.load(curr_file)
-    x = librosa.feature.melspectrogram(y=y, sr=sr)
-    with open(new_filenames[i], "wb") as f:
-        writer = csv.writer(f)
-        writer.writerows(x)
+# for file in os.listdir(os.fsencode(directory)):
+#     filename = directory + os.fsdecode(file)
+#     y, sr = librosa.load(filename)
+#     x = librosa.feature.melspectrogram(y=y, sr=sr)
+#     dat.append(x)
+
+# print(type(dat))
+# # print(x[1])
+# # print(x[6])
+# print(len(dat))
+# # print(x)
+# y = torch.Tensor(dat)
+# print(type(y))
+# print(y.size())
+
+lab_to_int = {
+    'D': 0,
+    'G': 1
+}
+
+class Spectrogram(object):
+    def __init__(self, path):
+        train_path = path + '/train/'
+        valid_path = path + '/valid/'
+        test_path = path + '/test/'
+
+        train  = []
+        for file in os.listdir(os.fsencode(train_path)):
+            filename = train_path +  os.fsdecode(file)
+            y, sr = librosa.load(filename)
+            x = librosa.feature.melspectrogram(y=y, sr=sr)
+            train.append([torch.Tensor(np.transpose(x)), torch.LongTensor([lab_to_int['D']])])  #CHANGE THIS TO ATTACH THE ACTUAL ANSWERS
+
+        #self.train = [torch.Tensor(train), ['D']*20] # CHANGE THIS TO ATTACH THE ACTUAL ANSWERS
+        self.train = train
+
+        valid  = []
+        for file in os.listdir(os.fsencode(valid_path)):
+            filename = valid_path + os.fsdecode(file)
+            y, sr = librosa.load(filename)
+            x = librosa.feature.melspectrogram(y=y, sr=sr)
+            valid.append(np.transpose(x))
+
+        self.valid = [torch.Tensor(valid), ['D']*20] # CHANGE THIS TO ATTACH THE ACTUAL ANSWERS
+
+        test  = []
+        for file in os.listdir(os.fsencode(test_path)):
+            filename = test_path + os.fsdecode(file)
+            y, sr = librosa.load(filename)
+            x = librosa.feature.melspectrogram(y=y, sr=sr)
+            test.append([torch.Tensor(np.transpose(x)), torch.LongTensor([lab_to_int['D']])])
+
+        self.test = test
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,1 +1,105 @@
+import torch 
+import torch.nn as nn
+import argparse
+
+import data
+import model
+
+
+# Hyper-parameters
+sequence_length = 23
+input_size = 128  #number of features per time step 
+hidden_size = 128  # can be anything
+num_classes = 2
+num_epochs = 2
+learning_rate = 0.003
+num_layers = 2
+num_classes = 2
+
+# Device configuration  -- WHAT DOES THIS DO?
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+parser = argparse.ArgumentParser(description='PyTorch consonant classifier')
+
+parser.add_argument('--epochs', type=int, default=40,
+                    help='upper epoch limit')
+
+parser.add_argument('--modelname', type=str, default='model.pt',
+                    help='path to save the final model')
+
+parser.add_argument('--nlayers', type=int, default=2,
+                    help='number of layers')
+
+parser.add_argument('--datadir', type=str, default='./data',
+                    help='data directory')
+
+# parser.add_argument('--batchsize', type=int, default=1,
+#                     help='upper epoch limit') #setting this to 1 will mean that it is not run in batches
+
+
+args = parser.parse_args()
+
+
+
+dat = data.Spectrogram(args.datadir)
+
+train_data = dat.train
+test_data = dat.test
+
+
+model = model.BiRNN(input_size, hidden_size, num_layers, num_classes).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+total_step = len(train_data)
+for epoch in range(num_epochs):
+    for i, (sound, label) in enumerate(train_data):  
+        sound = sound.reshape(1, sequence_length, input_size).to(device) #change this 1 to batch size if I want to implement batches in the future
+        label = label.to(device)  #gets the index of the label and writes to to device
+
+        #forward pass
+        output = model(sound)  
+        loss = criterion(output, label)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+# Test the model
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for sound, label in test_data:
+        sound = sound.reshape(1, sequence_length, input_size).to(device)
+        label = label.to(device)
+        output = model(sound)
+        _, predicted = torch.max(output.data, 1)
+        total += label.size(0)
+        correct += (predicted == label).sum().item()
+
+    print('Test Accuracy of the model {} %'.format(100 * correct / total))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
