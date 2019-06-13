ba53c64e0e2948f781d0bf9080954a518626f5f9
grushaprasad <grusha.prasad@gmail.com>
2019-06-13 11:22:14 -0400

    precursor spectrograms

--- make_spectrograms.py
+++ make_spectrograms.py
@@ -3,19 +3,35 @@ import numpy as np, math
 import glob, pickle, re, sys
 import os
 
+vowels = {
+    'a': 'AA',
+    'i': 'IY',
+    'u': 'UW',
+    'ae': 'AE'
+}
 
 stops = ['D', 'G']
 pattern = '^['+ ''.join(stops) +'][AEIOU]'
+lab_to_int = {
+    'D': 0,
+    'G': 1
+}
+
+
 #stop_cats = ['P', 'B', 'T', 'D', 'K', 'G']
+
+# Kaldi description:
+# "number of frames in the file: typically 25 ms frames shifted by 10ms each time"
 
 # spectrogram parameters
 sr_target   = 11025 # 11.025 kHz of Stephens & Holt (2011)
-n_fft       = 512
-hop_length  = int(n_fft/2)
+n_fft = int((sr_target*20)/1000)  # makes the window size roughly 25 ms. 
+hop_length = int((sr_target*10)/1000) # shifts the window by roughly 10 ms
 n_mels      = 64    # librosa default is 128
 eps         = 1.0e-8
 max_dur = 1.16 #seconds as computed by get_max_dur
 
+padding = 'unpadded'
 
 # gets the maximum duration of all 
 
@@ -37,8 +53,9 @@ def get_mu_sd(specs):
 def make_spectrogram(wavfile, offset=0.0, duration=None, log=True):
     snd,sr  = librosa.core.load(wavfile, sr=sr_target, offset=offset, duration=duration) 
     dur = librosa.get_duration(y=snd, sr=sr_target)
-    snd_padded = add_silence(snd, sr_target, dur, max_dur)
-    melspec = librosa.feature.melspectrogram(y=snd_padded, sr=sr_target, S=None, n_fft=n_fft, hop_length=hop_length, power=2.0, n_mels=n_mels)
+    if padding == 'padded':
+        snd = add_silence(snd, sr_target, dur, max_dur)
+    melspec = librosa.feature.melspectrogram(y=snd, sr=sr_target, S=None, n_fft=n_fft, hop_length=hop_length, power=2.0, n_mels=n_mels)
     logmelspec = np.log(melspec + eps)
     if log:
         return(logmelspec)
@@ -74,53 +91,58 @@ def make_spectrograms(wavfile, gridfile):
 
             syll = syll+str(word_indx)
             stop = stop_interval.text
-            vowel = vowel_interval.text
-            logmelspec = make_spectrogram(wavfile, start_time, dur)
-            results.append((logmelspec, syll, stop, vowel))  # [spectrogram, word, consonant, vowel]
+            vowel = vowel_interval.text[0:2]
+            if vowel in vowels.values():
+                logmelspec = make_spectrogram(wavfile, start_time, dur)
+                results.append((wavfile,logmelspec, syll, stop, vowel, lab_to_int[stop]))  # [spectrogram, word, consonant, vowel]
             #results.append((logmelspec, stop))
             #print (results)
             #sys.exit(0)
- 
+    print(len(results))
     return results
 
-# make spectrograms for all natural recordings, 
-# grouped by speaker
+# Make spectrograms for all natural recordings, 
+    ## grouped by speaker
 if 0:
     maindir = './ChodroffWilson2014'
     gridfiles = glob.glob(maindir+'/textgrid/*.TextGrid')
 
     for i,gridfile in enumerate(gridfiles):
+
         print('Processing %s of %s'%(i+1, len(gridfiles)))
         wavfile = re.sub('/textgrid/', '/wav/', gridfile)
         wavfile = re.sub('.TextGrid', '_edited.wav', wavfile)
         results = make_spectrograms(wavfile, gridfile)
 
-        pklfile = re.sub('/textgrid/', '/spectrograms/%s/'%n_mels, gridfile)
+        pklfile = re.sub('/textgrid/', '/spectrograms/%s/%smels/'%(padding,n_mels), gridfile)
         pklfile = re.sub('.TextGrid', '.pkl', pklfile)
+
+        #print(pklfile)
 
         with open(pklfile, 'wb') as f:
             pickle.dump(results, f)
         #sys.exit(0)
 
 
-#make spectrograms for all synthetic stimuli
-
 if 0:
     maindir = './SyntheticDG/wav/'
     #results = []
     ends = []
     middle = []
-    everything = []
 
     wavfiles = glob.glob(maindir+'*.wav')
 
     for f in wavfiles:
+
+        everything = []
+
         if len(re.findall('\d+', f)) > 0:  #ignores other files
             filenum = int(re.findall('\d+', f)[0])
-            if f[1] == '_':
-                vowel = f[0:1]
+            f_name = os.path.basename(f)
+            if f_name[1] == '_':
+                vowel = vowels[f_name[0:1]]
             else:
-                vowel = f[0:2]
+                vowel = vowels[f_name[0:2]]
 
             if filenum < 11:
                 lab = 'D'
@@ -128,123 +150,122 @@ if 0:
                 lab = 'G'
             
             logmelspec = make_spectrogram(f)
-            everything.append((logmelspec, 'NA', lab, vowel, f))
+            everything.append((f, logmelspec, 'NA', lab, vowel,lab_to_int[lab]))
 
             if filenum < 6 or filenum > 15:
-                ends.append((logmelspec, 'NA', lab, vowel, f))
+                ends.append((f, logmelspec, 'NA', lab, vowel,lab_to_int[lab]))
             else:
-                middle.append((logmelspec, 'NA', lab, vowel, f))
-
-            #results.append((logmelspec, lab))
-
-    pklfile = './SyntheticDG/spectrograms/' + 'SyntheticDG_%s_ends.pkl'%n_mels
-    with open(pklfile, 'wb') as f:
-            pickle.dump(ends, f)
+                middle.append((f, logmelspec, 'NA', lab, vowel,lab_to_int[lab]))
 
 
-    pklfile = './SyntheticDG/spectrograms/' + 'SyntheticDG_%s_middle.pkl'%n_mels
-    with open(pklfile, 'wb') as f:
-            pickle.dump(middle, f)
+            pklfile = re.sub('/wav/', '/spectrograms/%s/%smels/%s/'%(padding,n_mels,vowel), f)
+            pklfile = re.sub('.wav', '.pkl', pklfile)
 
-    pklfile = './SyntheticDG/spectrograms/' + 'SyntheticDG_%s_all.pkl'%n_mels
-    with open(pklfile, 'wb') as f:
-            pickle.dump(everything, f)
+            #print(pklfile)
+
+
+            with open(pklfile, 'wb') as f:
+                pickle.dump(everything, f)
 
 
 
-## Make spectrograms subtracted from precursor
-
-ntone = 21 # 21 sine-wav tones with unique frequencies that depend on condition:
-low_mean = np.arange(1300.0, 2300.0+50.0, 50.0) # mean = 1800 Hz
-high_mean = np.arange(2300.0, 3300.0+50.0, 50.0) # mean = 2800 Hz
-mid_mean_low_var = np.arange(1800.0, 2800.0+50.0, 50.0) # mean = 2300 Hz
-mid_mean_high_var = np.arange(1300.0, 3300.0+50.0, 100.0) # mean = 2300 Hz
-standard_freq = 2300.0 # standard tone at 2300 Hz
-tone_dur = 70.0/1000.0 # 70 ms duration for each tone
-isi_dur = 30.0/1000.0 # 30 ms silence between successive tones
-sil_dur = 50.0/1000.0 # 50 ms silence between standard tone and speech
-ramp_dur = 5.0/1000.0 # 5 ms onset and offset ramp for each tone
-sr = 10000.0 # 10 kHz sampling rate
-
-linear_ramp = np.linspace(0.0, 1.0, int(ramp_dur*sr))
-mask = np.ones(int((tone_dur - 2.0*ramp_dur)*sr))
-mask = np.concatenate((linear_ramp, mask, linear_ramp[::-1]))
-
-low_tones = [mask*librosa.core.tone(freq, sr, duration=tone_dur)[0:700] for freq in low_mean]
-high_tones = [mask*librosa.core.tone(freq, sr, duration=tone_dur)[0:700] for freq in high_mean]
-standard_tone = mask*librosa.core.tone(standard_freq, sr, duration=tone_dur)[0:700]
-
-
-np.random.shuffle(low_tones)
-np.random.shuffle(high_tones)
-isi = np.zeros(int(isi_dur * sr))
-sil = np.zeros(int(sil_dur * sr))
-
-low_condition = np.concatenate(
-    [np.concatenate((tone, isi)) for tone in low_tones] + [standard_tone,sil])
-high_condition = np.concatenate(
-    [np.concatenate((tone, isi)) for tone in high_tones] + [standard_tone, sil])
-
-melspec_low = librosa.feature.melspectrogram(y=low_condition, sr=sr, S=None, n_fft=n_fft, hop_length=hop_length, power=2.0, n_mels=n_mels)
-logmelspec_low = np.log(melspec_low + eps)
-mu_low, _ = get_mu_sd(logmelspec_low)
-
-melspec_high = librosa.feature.melspectrogram(y=high_condition, sr=sr, S=None, n_fft=n_fft, hop_length=hop_length, power=2.0, n_mels=n_mels)
-logmelspec_high = np.log(melspec_high + eps)
-mu_high, _ = get_mu_sd(logmelspec_high)
-
-
+## Make spectrograms for precursors
 if 1:
-    maindir = './SyntheticDG/wav/'
-    #results = []
-    low_subtracted = []
-    high_subtracted = []
-    #maxes = []
+    standard_freq = 2300.0
+    tone_dur = 70.0/1000.0 # 70 ms duration for each tone
+    isi_dur = 30.0/1000.0 # 30 ms silence between successive tones
+    sil_dur = 50.0/1000.0 # 50 ms silence between standard tone and speech
+    ramp_dur = 5.0/1000.0
+    sr = 11025
 
-    wavfiles = glob.glob(maindir+'*.wav')
+    offset = 0.098 
+    duration = 0.365
 
-    for f in wavfiles:
-        if len(re.findall('\d+', f)) > 0:  #ignores other files
-            filenum = int(re.findall('\d+', f)[0])
-            if f[1] == '_':
-                vowel = f[0:1]
-            else:
-                vowel = f[0:2]
+    rms_target = 0.06362659
 
-            if filenum < 11:
-                lab = 'D'
-            else:
-                lab = 'G'
-            
-            logmelspec = make_spectrogram(f)
-            #everything.append((logmelspec, 'NA', lab, vowel, f))
+    num_conds = 4
+    num_trials_per_cond = 10
 
-            # melspec = make_spectrogram(f, log= False)
-            # #maxes.append(np.amin(melspec))
+    stim_files = ['a_dg09_CV.wav', 'a_dg10_CV.wav', 'a_dg11_CV.wav', 'a_dg12_CV.wav', 'a_dg13_CV.wav','a_dg14_CV.wav','a_dg15_CV.wav','a_dg16_CV.wav']
 
-            logmelspec_high_subtracted = logmelspec - mu_high
-            #melspec_high_subtracted[melspec_high_subtracted < 0] = 0
-            #logmelspec_high_subtracted = np.log(melspec_high_subtracted + eps)
-            high_subtracted.append((logmelspec_high_subtracted, 'NA', lab, vowel, f))
+    def rms_amplitude(x):
+        rms = np.sqrt(np.mean(x**2.0))
+        return rms
 
-            logmelspec_low_subtracted = logmelspec - mu_low
-            #melspec_low_subtracted[melspec_low_subtracted < 0] = 0
-            #logmelspec_low_subtracted = np.log(melspec_low_subtracted + eps)
-            low_subtracted.append((logmelspec_low_subtracted, 'NA', lab, vowel, f))
+    def scale_rms(x, rms_target):
+        rms_x = rms_amplitude(x)
+        s = (rms_target / rms_x)
+        y = s*x
+        return y
 
-            logmelspec = make_spectrogram(f)
+    def create_precursor_freqs(l1,l2):
+        np.random.shuffle(l1)
+        np.random.shuffle(l2)
+        return(np.concatenate((l1, l2),0))
 
+    def create_precursors(l, tone_sr, tone_dur):   
+        linear_ramp = np.linspace(0.0, 1.0, int(ramp_dur*tone_sr))
+        mask = np.ones(int((tone_dur - 2.0*ramp_dur)*tone_sr))
+        mask = np.concatenate((linear_ramp, mask, linear_ramp[::-1]))
+        standard_tone = mask*librosa.core.tone(standard_freq, tone_sr, duration=tone_dur)[0:771] 
 
 
+        sil = np.zeros(int(sil_dur * tone_sr))
+        isi = np.zeros(int(isi_dur * tone_sr))
 
-    pklfile = './SyntheticDG/spectrograms/' + 'SyntheticDG_%s_all_high_subtracted.pkl'%n_mels
+        precursor_list = [0]*len(l)
+        for i, item in enumerate(l):
+
+            curr_tones = [mask*librosa.core.tone(freq, tone_sr, duration=tone_dur)[0:771] for freq in item]
+            curr_precursor = np.concatenate([np.concatenate((tone, isi)) for tone in curr_tones] + [standard_tone, sil])
+            precursor_list[i] = scale_rms(curr_precursor, rms_target)
+        return(precursor_list)
+
+    low = np.arange(800.0, 1800.0+100.0, 100.0) # mean 1300
+    mid = np.arange(1800.0, 2800.0+100.0, 100.0) # mean 2300
+    high = np.arange(2800.0, 3800.0+100.0, 100.0) # mean 3300
+
+    lowmid = [create_precursor_freqs(low, mid) for i in range(num_trials_per_cond*len(stim_files))]
+    midlow = [create_precursor_freqs(mid, low) for i in range(num_trials_per_cond*len(stim_files))]
+    highmid = [create_precursor_freqs(high, mid) for i in range(num_trials_per_cond*len(stim_files))]
+    midhigh = [create_precursor_freqs(mid, high) for i in range(num_trials_per_cond*len(stim_files))]
+
+    #lowmid = [create_precursor_freqs(low, mid)]
+
+    # This is taking just just the first precursor. In the future maybe save the log melspec for a bunch of precursors?
+    lowmid_precursors = create_precursors(lowmid, sr, tone_dur)
+    lowmid_melspec = librosa.feature.melspectrogram(y=lowmid_precursors[0], sr=sr_target, S=None, n_fft=n_fft, hop_length=hop_length, power=2.0, n_mels=n_mels)
+    lowmid_logmelspec = np.log(lowmid_melspec + eps)
+
+    midlow_precursors = create_precursors(midlow, sr, tone_dur)
+    midlow_melspec = librosa.feature.melspectrogram(y=midlow_precursors[0], sr=sr_target, S=None, n_fft=n_fft, hop_length=hop_length, power=2.0, n_mels=n_mels)
+    midlow_logmelspec = np.log(midlow_melspec + eps)
+
+    highmid_precursors = create_precursors(highmid, sr, tone_dur)
+    highmid_melspec = librosa.feature.melspectrogram(y=highmid_precursors[0], sr=sr_target, S=None, n_fft=n_fft, hop_length=hop_length, power=2.0, n_mels=n_mels)
+    highmid_logmelspec = np.log(highmid_melspec + eps)
+
+    midhigh_precursors = create_precursors(midhigh, sr, tone_dur)
+    midhigh_melspec = librosa.feature.melspectrogram(y=midhigh_precursors[0], sr=sr_target, S=None, n_fft=n_fft, hop_length=hop_length, power=2.0, n_mels=n_mels)
+    midhigh_logmelspec = np.log(midhigh_melspec + eps)
+
+
+    pklfile = './precursors/%s/%smels/'%(padding,n_mels) + 'lowmid.pkl'
     with open(pklfile, 'wb') as f:
-            pickle.dump(high_subtracted, f)
+        pickle.dump(lowmid_logmelspec, f)
 
-
-    pklfile = './SyntheticDG/spectrograms/' + 'SyntheticDG_%s_all_low_subtracted.pkl'%n_mels
+    pklfile = './precursors/%s/%smels/'%(padding,n_mels) + 'midlow.pkl'
     with open(pklfile, 'wb') as f:
-            pickle.dump(low_subtracted, f)
+        pickle.dump(midlow_logmelspec, f)
+
+    pklfile = './precursors/%s/%smels/'%(padding,n_mels) + 'highmid.pkl'
+    with open(pklfile, 'wb') as f:
+        pickle.dump(highmid_logmelspec, f)
+
+    pklfile = './precursors/%s/%smels/'%(padding,n_mels) + 'midhigh.pkl'
+    with open(pklfile, 'wb') as f:
+        pickle.dump(midhigh_logmelspec, f)
 
 
 
+   
